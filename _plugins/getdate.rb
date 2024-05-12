3375f3cafa908785961cbd021fcf137f92b08584
Henrik Helmers <helmers@me.com>
2024-05-12 15:23:58 +0200

    refactor: streamline Gemfile and config, remove unused plugins

--- _plugins/getdate.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll
-module GetDateFilter
-def getDate(item, par1, par2)
-    par1 ? par1 : par2.gsub(':', '-')
-end
-end
-end
-Liquid::Template.register_filter(Jekyll::GetDateFilter)
--- _plugins/gettitle.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll
-module GetTitleFilter
-def getTitle(item, par1, par2)
-    par1 ? par1 : par2
-end
-end
-end
-Liquid::Template.register_filter(Jekyll::GetTitleFilter)
